c97ddaed845afd8fd2fd624258e02a47a14f9928
Kamil Dybek <kam.dyb1@gmail.com>
2024-08-09 21:49:22 +0200

    added automatic packer installation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,17 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
 
-return require('packer').startup(function()
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'tomasr/molokai'
     use {
@@ -44,4 +55,8 @@ return require('packer').startup(function()
         requires = { { 'nvim-lua/plenary.nvim' } },
     }
     use 'cdelledonne/vim-cmake'
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
